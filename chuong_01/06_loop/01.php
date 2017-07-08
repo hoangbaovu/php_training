76e0cffe5379399d8a20d581e2b90e7810d5e220
hoangbaovu <baovuhbv@gmail.com>
2017-07-08 18:31:26 +0700

    First

---
+++ chuong_01/06_loop/01.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chép phạt</title>
+    <style>
+        * {
+            margin: 0; padding: 0;
+        }
+
+        .content {
+            margin: 0 auto;
+            width: 400px;
+        }
+
+        .content .row {
+            margin-top: 20px;
+        }
+        .content .row span {
+            display: inline-block;
+            width: 100px;
+            text-align: left;
+        }
+
+        .content .result {
+            margin-top: 20px;
+        }
+
+        .content .result img {
+            width: 100px;
+            height: 100px;
+            float: left;
+        }
+
+        .content .result p {
+            height: 100px;
+            line-height: 100px;
+            text-align: justify;
+        }
+
+        .btn {
+            background: red;
+            border: 1px solid red;
+            padding: 10px;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+
+?>
+    <div class="content">
+        <h1>Chép phạt</h1>
+    </div>
+</body>
+</html>
