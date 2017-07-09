332ae5b06e0204caac0f2df5026111b63fe95fa5
hoangbaovu <baovuhbv@gmail.com>
2017-07-09 14:26:30 +0700

    Learn do while

---
+++ chuong_01/06_loop/04.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chép phạt</title>
+    <style>
+        * {
+            margin: 0; padding: 0;
+        }
+
+        .content {
+            background: url("images/notebook_background.jpg") no-repeat;
+            min-height: 200px;
+            margin: 20px auto;
+            width: 600px;
+            padding: 20px;
+        }
+
+        .content h1 {
+            text-align: center;
+        }
+
+        .list {
+            list-style: decimal inside;
+            margin: 35px 80px;
+        }
+
+        .list__item {
+            margin-top: 6px;
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
+        <h1>Chép phạt</h1>
+        <ul class="list">
+            <?php
+                $i = 1;
+
+                do {
+                    echo '<li class="list__item">Em hứa sẽ làm bài tập ở nhà đầy đủ</li>';
+                    $i++;
+                } while ($i <= 10);
+            ?>
+        </ul>
+    </div>
+</body>
+</html>
