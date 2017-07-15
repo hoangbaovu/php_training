1024326a1a5335965cfb011c47610f682343d542
hoangbaovu <baovuhbv@gmail.com>
2017-07-15 17:44:02 +0700

    Hàm một thông số

---
+++ chuong_01/07_function/04.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
+
+        .content {
+            border: 1px solid #222;
+            height: auto;
+            margin: 20px auto;
+            padding: 20px;
+            width: 800px;
+        }
+
+        .content .div {
+            border: 1px solid red;
+            padding: 5px;
+            text-align: center;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
+
+    <!-- Hàm một thông số-->
+    <?php
+        function createBox($content){
+            $result = '<div class="div" style="width: 300px; height: 200px">';
+            $result .= '<p>'. $content .' <span>300x200</span></p>';
+            $result .= '</div>';
+            return $result;
+        }
+        
+        $boxA = createBox("Box A");
+        $boxB = createBox("Box B");
+        echo $boxA;
+        echo $boxB;
+    ?>
+    </div>
+</body>
+</html>
