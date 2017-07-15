543056848013e882a68f87b725bc240655eda871
hoangbaovu <baovuhbv@gmail.com>
2017-07-16 00:55:02 +0700

    Tính tổng các chữ số

---
+++ chuong_01/07_function/09.php
@@ -0,0 +1,23 @@
+<?php
+    // Tính tổng các chữ số
+
+    function sumDigit($number) {
+        // 232 -> 2: sum = 2, n = 23
+        // 23  -> 3: sum = 5, n = 2
+        // 2   -> 2: sum = 7, n = 0
+
+
+        $sum = 0;
+
+        while($number > 0) {
+            $digit = $number % 10;
+            $sum += $digit;
+            $number = ($number - $digit) / 10;
+        }
+        return $sum;
+    }
+
+    $result = sumDigit(332);
+
+    echo $result;
+?>
