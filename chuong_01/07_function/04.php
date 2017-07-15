09a8fa1a8741a52da3d2628313877980654e016a
hoangbaovu <baovuhbv@gmail.com>
2017-07-15 17:51:52 +0700

    Hàm nhiều thông số Gán giá trị mặc định

--- chuong_01/07_function/04.php
+++ chuong_01/07_function/04.php
@@ -30,20 +30,35 @@
 <body>
     <div class="content">
 
-    <!-- Hàm một thông số-->
-    <?php
-        function createBox($content){
-            $result = '<div class="div" style="width: 300px; height: 200px">';
-            $result .= '<p>'. $content .' <span>300x200</span></p>';
+    <!-- Hàm một thông số -->
+    <!-- <?php
+        // function createBox($content){
+        //     $result = '<div class="div" style="width: 300px; height: 200px">';
+        //     $result .= '<p>'. $content .' <span>300x200</span></p>';
+        //     $result .= '</div>';
+        //     return $result;
+        // }
+        
+        // $boxA = createBox("Box A");
+        // $boxB = createBox("Box B");
+        // echo $boxA;
+        // echo $boxB;
+    ?> -->
+
+    <!-- Hàm nhiều thông số & gán giá trị mặc định -->
+     <?php
+        function createBox($content, $width = 150, $height = 150){
+            $result = '<div class="div" style="width: '.$width.'px; height: '.$height.'px">';
+            $result .= '<p>'. $content .' <span>'.$width.'x'.$height.'</span></p>';
             $result .= '</div>';
             return $result;
         }
         
-        $boxA = createBox("Box A");
+        $boxA = createBox("Box A", 300, 200);
         $boxB = createBox("Box B");
         echo $boxA;
         echo $boxB;
-    ?>
+    ?> 
     </div>
 </body>
 </html>
