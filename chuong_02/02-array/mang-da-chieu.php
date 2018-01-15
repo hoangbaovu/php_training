6b3752a2740d871878c9b949dffbfedcb9d99457
hoangbaovu <baovuhbv@gmail.com>
2018-01-15 22:52:54 +0700

    In mảng đa chiều

--- chuong_02/02-array/mang-da-chieu.php
+++ chuong_02/02-array/mang-da-chieu.php
@@ -18,22 +18,46 @@
   $students["SV03"] = array("name" => "Marry", "sex" => 0, "score" => array(7, 8, 9));
 
   // Cách 2
-  $student = array(
-    "SV01" => array("name"  => "John",
-                    "sex"   => 1,
-                    "score" => array(4, 5, 6)
-              ),
-    "SV02" => array("name"  => "Doe",
-                    "sex"   => 0,
-                    "score" => array(5, 6, 7)
-              ),
-    "SV03" => array("name"  => "Marry",
-                    "sex"   => 0,
-                    "score" => array(7, 8, 9)
-              ),
-  );
+  // $student = array(
+  //   "SV01" => array("name"  => "John",
+  //                   "sex"   => 1,
+  //                   "score" => array(4, 5, 6)
+  //             ),
+  //   "SV02" => array("name"  => "Doe",
+  //                   "sex"   => 0,
+  //                   "score" => array(5, 6, 7)
+  //             ),
+  //   "SV03" => array("name"  => "Marry",
+  //                   "sex"   => 0,
+  //                   "score" => array(7, 8, 9)
+  //             ),
+  // );
 
   echo '<pre>';
-  print_r($student);
+  print_r($students["SV01"]["name"]);
+  echo '</pre>';
+
+  // Tên sinh viên 01
+
+  echo $students["SV01"]["name"];
+
+  echo "<br>";
+
+  // Điểm môn thứ 2 của SV02
+
+  echo $students["SV02"]["score"][1];
+
+  echo "<br>";
+
+  // Thay đổi tên của SV03 thành Anne
+
+  $students["SV03"]["name"] = "Anne";
+
+  // Thay đổi điểm của SV03 môn 2 thành 10;
+
+  $studens["SV03"]["score"][1] = 10;
+
+  echo '<pre>';
+  print_r($students);
   echo '</pre>';
 ?>
