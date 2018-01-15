07de7830314da67cba434d987d6f1028f2811627
hoangbaovu <baovuhbv@gmail.com>
2018-01-15 22:15:48 +0700

    Mảng kết hợp

---
+++ chuong_02/02-array/mang-ket-hop.php
@@ -0,0 +1,23 @@
+<?php
+  /*
+    3. Khai báo và sử dụng mảng kết hợp
+    - Mảng kết hợp là mảng mà các chỉ sổ của các phần tử có thể là chuỗi hoặc số
+      (Mảng kết hợp còn gọi là mảng không liên tục)
+    - In danh sách các phần tử trong mảng kết hợp: foreach
+  */
+
+  // $courses              = array();
+  // $courses["php"]       = "PHP";      // key: php
+  // $courses["zend"]      = "Zend";     // key: zend
+  // $courses["laravel"]   = "Laravel";  // key: laravel
+  // $courses[]            = "Item 1";   // key: 0
+  // $courses[]            = "Item 2";   // key: 1
+
+  $courses = array("php" => "PHP", "zend" => "Zend", "laravel" => "Laravel", 0 => "Item 1", 1 => "Item 2" );
+
+  echo '<pre>';
+  print_r($courses);
+  echo '</pre>';
+
+  echo $courses[0];
+?>
