519503bbb31e655870768473ee1eb0bdaa34c713
hoangbaovu <baovuhbv@gmail.com>
2018-01-15 22:44:13 +0700

    Mảng đa chiều

--- chuong_02/02-array/mang-da-chieu.php
+++ chuong_02/02-array/mang-da-chieu.php
@@ -10,12 +10,30 @@
   // Quản lý thông tin sinh viên
   // SV1 name, sex, score
 
+  // Cách 1
+
   $students = array();
   $students["SV01"] = array("name" => "John", "sex" => 1, "score" => array(4, 5, 6));
   $students["SV02"] = array("name" => "Doe", "sex" => 0, "score" => array(5, 6, 7));
   $students["SV03"] = array("name" => "Marry", "sex" => 0, "score" => array(7, 8, 9));
 
+  // Cách 2
+  $student = array(
+    "SV01" => array("name"  => "John",
+                    "sex"   => 1,
+                    "score" => array(4, 5, 6)
+              ),
+    "SV02" => array("name"  => "Doe",
+                    "sex"   => 0,
+                    "score" => array(5, 6, 7)
+              ),
+    "SV03" => array("name"  => "Marry",
+                    "sex"   => 0,
+                    "score" => array(7, 8, 9)
+              ),
+  );
+
   echo '<pre>';
-  print_r($students);
+  print_r($student);
   echo '</pre>';
 ?>
