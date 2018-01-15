fb0de34f5716aecdabcbadf59df0c1b8b7bf260b
hoangbaovu <baovuhbv@gmail.com>
2018-01-15 18:54:54 +0700

    Giới thiệu array

---
+++ chuong_02/02-array/01.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  1. Hiểu như thế nào về array trong PHP
+
+  - Mảng là một biến đặc biệt và có thể lưu trữ nhiều giá trị
+  - Một biến thông thường chỉ chứa một giá trị duy nhất,
+    nếu chúng ta muốn chứa nhiều giá trị trong một biến thì biến đó phải là một mảng
+  - Trong PHP có 3 loại mảng: mảng số nguyên, mảng kết hợp và mảng đa chiều.
+/*
+?>
