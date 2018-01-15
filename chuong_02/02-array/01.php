0045004904fb6d0d25a218b9fdc98998de654b36
hoangbaovu <baovuhbv@gmail.com>
2018-01-15 18:57:53 +0700

    Dem so phan tu trong mang

--- chuong_02/02-array/01.php
+++ chuong_02/02-array/01.php
@@ -6,5 +6,17 @@
   - Một biến thông thường chỉ chứa một giá trị duy nhất,
     nếu chúng ta muốn chứa nhiều giá trị trong một biến thì biến đó phải là một mảng
   - Trong PHP có 3 loại mảng: mảng số nguyên, mảng kết hợp và mảng đa chiều.
-/*
+*/
+
+  $courses    = array();
+  $courses[]  = "PHP";
+  $courses[]  = "Zend";
+  $courses[]  = "Laravel";
+
+  // Đếm số phần tử trong mảng
+
+  $length = count($courses);
+
+  echo $length;
+
 ?>
